8806e9fc4ec655fc22437c76cd9afaf31897bcf0
ckkim817 <ckkim817@konkuk.ac.kr>
2024-06-03 03:10:40 +0900

    Refresh Token 만료 시 ErrorMessage 추가

--- 6thSeminar/springPractice/src/main/java/org/sopt/springPractice/auth/redis/service/TokenService.java
+++ 6thSeminar/springPractice/src/main/java/org/sopt/springPractice/auth/redis/service/TokenService.java
@@ -6,7 +6,6 @@ import org.sopt.springPractice.auth.redis.repository.RedisTokenRepository;
 import org.sopt.springPractice.auth.redis.service.dto.AccessTokenDTO;
 import org.sopt.springPractice.common.dto.ErrorMessage;
 import org.sopt.springPractice.common.jwt.JwtTokenProvider;
-import org.sopt.springPractice.common.jwt.JwtValidationType;
 import org.sopt.springPractice.exception.UnauthorizedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,17 +20,10 @@ public class TokenService {
     @Transactional
     public AccessTokenDTO reissueAccessToken(Long userId) {
         Token token = redisTokenRepository.findById(userId).orElseThrow(
-                () -> new UnauthorizedException(ErrorMessage.REFRESH_TOKEN_NOT_FOUND)
+                () -> new UnauthorizedException(ErrorMessage.MEMBER_NOT_FOUND)
         );
 
-        JwtValidationType validationType = jwtTokenProvider.validateToken(token.getRefreshToken());
-
-        if (validationType == JwtValidationType.EXPIRED_JWT_TOKEN) {
-            throw new UnauthorizedException(ErrorMessage.JWT_UNAUTHORIZED_EXCEPTION);
-        } else if (validationType != JwtValidationType.VALID_JWT) {
-            throw new UnauthorizedException(ErrorMessage.JWT_UNAUTHORIZED_EXCEPTION);
-        }
-
+        jwtTokenProvider.validateToken(token.getRefreshToken());
         String newAccessToken = jwtTokenProvider.newAccessToken(token.getRefreshToken());
 
         return AccessTokenDTO.of(newAccessToken);
